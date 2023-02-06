7502a9114826a1018dc4d6ac9850d2c94bc7e533
stanislowskij <stani152@morris.umn.edu>
2023-02-05 19:43:14 -0600

    product details now display with the component
    /products/{id} urls will now display product name, price and description for a given product

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    // I really wish I understood what is happening here. 
+    // I'm gonna need more TypeScript experience before I get any of this.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
 }
